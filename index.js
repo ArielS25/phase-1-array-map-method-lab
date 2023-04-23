e286234005e68a55609c2b70f80dcc21345f3e9e
Jorge Ariel Sanchez Leon <qwertyice2552@gmail.com>
2023-04-23 14:55:15 -0700

    lab complete

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
   'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
+'The Test Driven Development Workflow',
   'What is NaN and how Can we Check for it',
   'What is the difference between stopPropagation and preventDefault?',
   'Immutable State and Pure Functions',
@@ -10,7 +10,12 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
-const titleCased = () => {
-  return tutorials
+function titleCased(){
+  return tutorials.map(function(tutorial) {
+    let words = tutorial.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+    return words.join(" ");
+  });
 }
